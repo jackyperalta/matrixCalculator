5bfc43a3c7d3caadf278a0b9218c08320b7a6f65
alo-beep <lopeza1829@gmail.com>
2021-04-29 01:22:28 -0700

    initial commit for matrixCalculator.rb
    added file to repo

---
+++ matrixCalculator.rb
@@ -0,0 +1,17 @@
+   # course: cmps3500
+   # CLASS Project
+   # RUBY IMPLEMENTATION OF A CUSTOM MATRIX CALCULATION
+   # date: 4/24/2021
+   # Student 1: Jacqueline Peralta
+   # Student 2: Angel Lopez
+   # Student 3: 
+   # description: Implementation of a matrix calculator
+
+require 'tk'
+
+root = TkRoot.new { title "Hello, World!" }
+TkLabel.new(root) do
+    text 'Hello, World!'
+    pack { padx 15 ; pady 15; side 'left' }
+end
+Tk.mainloop

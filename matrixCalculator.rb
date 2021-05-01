a60ba705a707a76c0d79634c8e2bbac9f59886d3
Aldair <amartinez238@csub.edu>
2021-05-01 15:42:16 -0700

    Added 3 functions, matrix no longer set size

--- matrixCalculator.rb
+++ matrixCalculator.rb
@@ -51,20 +51,88 @@ Tk.mainloop
 #Code for storing csv to array/matrix
 require 'csv'
 
-row, col = 8, 8
-index = 0
-puts "Matrix A\n"
-matrixA = Array.new(row){Array.new(col)}
-#for i in 0..9
-CSV.foreach("A.csv") do |row|
-    matrixA[index] = row
-    index = index + 1
+
+#function adds 2 matrices
+def add(matrixA, matrixB)
+  #take matrix size and store into row & col
+  row, col = $matA_width, $matB_height
+  #declare 3rd matrix with size of matrix width and height
+  matrixC = Array.new(row){Array.new(col)}
+  for i in 0..col-1
+    for j in 0..row-1
+      #add and store into 3rd matrix
+      matrixC[i][j] = matrixA[i][j] + matrixB[i][j]
+    end
+  end
+print "\nMatrix A + Matrix B\n", matrixC
 end
-#end
+
+#function subtracts 2 matrices
+def subtract(matrixA, matrixB)
+  #take matrix size and store into row & col
+  row, col = $matA_width, $matB_height
+  #declare 3rd matrix with size of matrix width and height
+  matrixC = Array.new(row){Array.new(col)}
+  for i in 0..col-1
+    for j in 0..row-1
+      #subtract and store into 3rd matrix
+      matrixC[i][j] = matrixA[i][j] - matrixB[i][j]
+    end
+  end
+print "\nMatrix A - Matrix B\n", matrixC
+end
+
+
+#fuction for matrix multiplication
+def multiply(matrixA, matrixB)
+  row, col = $matA_width, $matB_height
+      #create a 3rd matrix
+      #3rd matrix will have size of matrix A's column
+      # and matrix B's row.
+  matrixC = Array.new(matrixA.length){Array.new(matrixB[0].length)}
+ 
+  for i in 0..col - 1
+    for j in 0..row - 1
+      matrixC[i][j] = 0  #set all to 0 initially
+    end
+  end
+
+  for i in 0..matrixC.length - 1
+    for j in 0..matrixC[0].length - 1
+      for k in 0..matrixA[0].length - 1
+        #multiplication of matrices is added to 3rd matrix
+        matrixC[i][j] += matrixA[i][k] * matrixB[k][j]
+      end
+    end
+  end
+  print "\nMatrix A * Matrix B\n", matrixC
+end
+
+
+row, col = 0, 0
+index = 0
+
+#create matrix
+matrixA = Array.new(row){Array.new(col)}
+
+puts "Matrix A\n"
+#read in matrix A file
+CSV.foreach("A.csv") do |row|
+  matrixA[index] = row
+  index = index + 1
+  #print "Index: ", index, "  row: ", row, "\n"
+end
 print matrixA
 
-puts "\n\nMatrix B\n"
+#store height and width of matrix A
+$matA_height = matrixA.length  #array.length gets you height
+$matA_width = matrixA[0].length #array[0].length gets you width
+print "\nMatrix A has a size of ", $matA_height, "x", $matA_width, "\n"
+
+
 index = 0
+puts "\n\nMatrix B\n"
+#read in matrix B file
 matrixB = Array.new(row){Array.new(col)}
   CSV.foreach("B.csv") do |row|
     matrixB[index] = row
@@ -72,7 +140,34 @@ matrixB = Array.new(row){Array.new(col)}
   end
 print matrixB
 
-string_num = matrixB[0][0]
-print "\nstring num: ", string_num
-num = string_num
-print "\nnum: ", num
+#store height and width of matrix B
+$matB_height = matrixB.length
+$matB_width = matrixB[0].length
+print "\nMatrix B has a size of ", $matB_height, "x", $matB_width, "\n"
+
+print "\n\n"
+#convert strings to ints
+for i in 0..$matA_height - 1
+  for j in 0..$matA_width - 1
+    matrixA[i][j] = matrixA[i][j].to_i
+    matrixB[i][j] = matrixB[i][j].to_i
+  end
+end
+#TEMPORARY SOLUTION for index [0][0] issue
+matrixA[0][0] = 66
+matrixB[0][0] = 51
+
+#function calls
+add(matrixA, matrixB)
+subtract(matrixA, matrixB)
+multiply(matrixA, matrixB)
+
+
+#string_num = matrixB[0][0]
+#print "\nstring num: ", string_num
+#num = string_num
+#print "\nnum: ", num
+#print "\n"
+#result = matrixB[0][0] + matrixA[0][0]
+
+#print result

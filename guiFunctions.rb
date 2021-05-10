3e9924307129023a328c90c90bde1f7c0cdab611
Angel O. Lopez <71157556+alo-beep@users.noreply.github.com>
2021-05-10 00:35:15 -0700

    Moved functions to separate file, made add work with button

--- guiFunctions.rb
+++ guiFunctions.rb
@@ -62,7 +62,15 @@ def btn_unaryPowerB_click
 end
 # start of binary function buttons
 def btn_binaryAplusB_click
-
+    rowA = findRow($matrixA)
+    colA = findCol($matrixA)
+    rowB = findRow($matrixB)
+    colB = findCol($matrixB)
+    # if both are the same size, continue
+    if (rowA == rowB && colA == colB)
+        matrixC = add($matrixA, $matrixB, rowA, colA)
+        insert_MatrixOut(matrixC)
+    end
 end
 
 def btn_binaryAminusB_click
@@ -83,55 +91,51 @@ end
 
 def btn_binaryAcopytoB_click
     $matrixB = $matrixA
-    insert_MatrixB()
+    insert_MatrixB($matrixB)
 end
 
 def btn_binaryBcopytoA_click
     $matrixA = $matrixB
-    insert_MatrixA()
+    insert_MatrixA($matrixA)
 end
 
 def btn_binaryAswaptoB_click
     matrixtmp = $matrixA
     $matrixA = $matrixB
     $matrixB = matrixtmp
-    insert_MatrixA()
-    insert_MatrixB()
+    insert_MatrixA($matrixA)
+    insert_MatrixB($matrixB)
 end
 
 # gathering input from csv file functions
 require 'csv'
 def btn_loadMatrixA_click
-    row, col = 8, 8
-    index = 0
+    row, col = 0, 0
     matrixAtmp = Array.new(row){Array.new(col)}
     CSV.foreach($matrixAFileName) do |row|
-        matrixAtmp[index] = row
-        index = index + 1
+        matrixAtmp.push(row)
     end
     $matrixA = matrixAtmp
-    insert_MatrixA()
+    insert_MatrixA($matrixA)
 end
 
 def btn_loadMatrixB_click
-    row, col = 8, 8
-    index = 0
+    row, col = 0, 0
     matrixBtmp = Array.new(row){Array.new(col)}
     CSV.foreach($matrixBFileName) do |row|
-        matrixBtmp[index] = row
-        index = index + 1
+        matrixBtmp.push(row)
     end
     $matrixB = matrixBtmp
-    insert_MatrixB()
+    insert_MatrixB($matrixB)
 end
 
 # insert Matrix's into their text boxes
-def insert_MatrixA
+def insert_MatrixA (matrix)
     $text_MatrixA['state'] = 'normal'
     $text_MatrixA.delete(1.0, 10.40)
     i = 1
     j = 0
-    $matrixA.each do |row|
+    matrix.each do |row|
         row.each do |element|
             index = i.to_s + "." + j.to_s
             $text_MatrixA.insert(index , element)
@@ -147,12 +151,12 @@ def insert_MatrixA
     $text_MatrixA['state'] = 'disabled'
 end
 
-def insert_MatrixB
+def insert_MatrixB (matrix)
     $text_MatrixB['state'] = 'normal'
     $text_MatrixB.delete(1.0, 10.40)
     i = 1
     j = 0
-    $matrixB.each do |row|
+    matrix.each do |row|
         row.each do |element|
             index = i.to_s + "." + j.to_s
             $text_MatrixB.insert(index , element)
@@ -168,12 +172,12 @@ def insert_MatrixB
     $text_MatrixB['state'] = 'disabled'
 end
 
-def insert_MatrixOut
+def insert_MatrixOut (matrix)
     $text_MatrixOut['state'] = 'normal'
     $text_MatrixOut.delete(1.0, 10.40)
     i = 1
     j = 0
-    $matrixB.each do |row|
+    matrix.each do |row|
         row.each do |element|
             index = i.to_s + "." + j.to_s
             $text_MatrixOut.insert(index , element)
@@ -187,4 +191,107 @@ def insert_MatrixOut
         i = i + 1
     end
     $text_MatrixOut['state'] = 'disabled'
+end
+
+def findRow(matrix_input)
+    rowCount = 0
+    # start finding row
+    matrix_input.each do |row|
+        rowCount = rowCount + 1
+    end
+    return rowCount
+end
+
+def findCol(matrix_input)
+    colCount = 0
+    # start finding col
+    matrix_input.each do |row|
+        row.each do |element|
+            colCount = colCount + 1
+        end
+        return colCount
+    end
+end
+
+#function adds 2 matrices
+def add(matrixA, matrixB, row, col)
+  #take matrix size and store into row & col
+  #declare 3rd matrix with size of matrix width and height
+  matrixC = Array.new(row){Array.new(col)}
+  for i in 0..col-1
+    for j in 0..row-1
+      #add and store into 3rd matrix
+      matrixC[i][j] = matrixA[i][j] + matrixB[i][j]
+    end
+  end
+print "\nMatrix A + Matrix B\n", matrixC
+return matrixC
+end
+
+#function subtracts 2 matrices
+def subtract(matrixA, matrixB) #A and B must be same size
+  #take matrix size and store into row & col
+  row, col = $matA_width, $matB_height
+  #declare 3rd matrix with size of matrix width and height
+  matrixC = Array.new(row){Array.new(col)}
+  for i in 0..col-1
+    for j in 0..row-1
+      #subtract and store into 3rd matrix
+      matrixC[i][j] = matrixA[i][j] - matrixB[i][j]
+    end
+  end
+print "\nMatrix A - Matrix B\n", matrixC
+end
+
+#fuction for matrix multiplication
+def multiply(matrixA, matrixB) #A's columns must equal B's rows
+  row, col = $matA_width, $matB_height
+      #create a 3rd matrix
+      #3rd matrix will have size of matrix A's column
+      # and matrix B's row.
+  matrixC = Array.new(matrixA.length){Array.new(matrixB[0].length)}
+
+  for i in 0..col - 1
+    for j in 0..row - 1
+      matrixC[i][j] = 0  #set all to 0 initially
+    end
+  end
+
+  for i in 0..matrixC.length - 1
+    for j in 0..matrixC[0].length - 1
+      for k in 0..matrixA[0].length - 1
+        #multiplication of matrices is added to 3rd matrix
+        matrixC[i][j] += matrixA[i][k] * matrixB[k][j]
+      end
+    end
+  end
+  print "\nMatrix A * Matrix B\n", matrixC
+end
+
+#Identity matrix function
+def identity(size)  #matrix must be square
+  matrixC = Array.new(size){Array.new(size)}
+  for i in 0..size - 1
+    for j in 0..size - 1
+      if i == j
+        matrixC[i][j] = 1
+      else
+        matrixC[i][j] = 0
+      end
+    end
+  end
+  print "\n\nIdentity matrix: ", matrixC, "\n"
+end
+
+def transpose(matrix, height, width)
+  #declare 3rd matrix with size of matrix width and height
+  matrixC = Array.new(width){Array.new(height)}
+  for i in 0..height-1
+    for j in 0..width-1
+      #arrange the values to resemble transpose
+      #store result in 2D array
+      matrixC[j][i] = matrix[i][j]
+    end
+  end
+  print "\nTranspose of matrix ", matrixC,"\n"
 end
--- matrixCalculator.rb
+++ matrixCalculator.rb
@@ -36,94 +36,6 @@ Tk.mainloop
 #Code for storing csv to array/matrix
 require 'csv'
 
-
-#function adds 2 matrices
-def add(matrixA, matrixB)
-  #take matrix size and store into row & col
-  row, col = $matA_width, $matB_height
-  #declare 3rd matrix with size of matrix width and height
-  matrixC = Array.new(row){Array.new(col)}
-  for i in 0..col-1
-    for j in 0..row-1
-      #add and store into 3rd matrix
-      matrixC[i][j] = matrixA[i][j] + matrixB[i][j]
-    end
-  end
-print "\nMatrix A + Matrix B\n", matrixC
-end
-
-#function subtracts 2 matrices
-def subtract(matrixA, matrixB) #A and B must be same size
-  #take matrix size and store into row & col
-  row, col = $matA_width, $matB_height
-  #declare 3rd matrix with size of matrix width and height
-  matrixC = Array.new(row){Array.new(col)}
-  for i in 0..col-1
-    for j in 0..row-1
-      #subtract and store into 3rd matrix
-      matrixC[i][j] = matrixA[i][j] - matrixB[i][j]
-    end
-  end
-print "\nMatrix A - Matrix B\n", matrixC
-end
-
-
-#fuction for matrix multiplication
-def multiply(matrixA, matrixB) #A's columns must equal B's rows
-  row, col = $matA_width, $matB_height
-      #create a 3rd matrix
-      #3rd matrix will have size of matrix A's column
-      # and matrix B's row.
-  matrixC = Array.new(matrixA.length){Array.new(matrixB[0].length)}
-
-  for i in 0..col - 1
-    for j in 0..row - 1
-      matrixC[i][j] = 0  #set all to 0 initially
-    end
-  end
-
-  for i in 0..matrixC.length - 1
-    for j in 0..matrixC[0].length - 1
-      for k in 0..matrixA[0].length - 1
-        #multiplication of matrices is added to 3rd matrix
-        matrixC[i][j] += matrixA[i][k] * matrixB[k][j]
-      end
-    end
-  end
-  print "\nMatrix A * Matrix B\n", matrixC
-end
-
-
-#Identity matrix function
-def identity(size)  #matrix must be square
-  matrixC = Array.new(size){Array.new(size)}
-  for i in 0..size - 1
-    for j in 0..size - 1
-      if i == j
-        matrixC[i][j] = 1
-      else
-        matrixC[i][j] = 0
-      end
-    end
-  end
-  print "\n\nIdentity matrix: ", matrixC, "\n"
-end
-
-def transpose(matrix, height, width)
-  #declare 3rd matrix with size of matrix width and height
-  matrixC = Array.new(width){Array.new(height)}
-  for i in 0..height-1
-    for j in 0..width-1
-      #arrange the values to resemble transpose
-      #store result in 2D array
-      matrixC[j][i] = matrix[i][j]
-    end
-  end
-  print "\nTranspose of matrix ", matrixC,"\n"
-end
-
-
-
 row, col = 0, 0
 index = 0
 
@@ -173,8 +85,8 @@ end
 #matrixB[0][0] = 51
 
 #function calls
-add(matrixA, matrixB)
-subtract(matrixA, matrixB)
-multiply(matrixA, matrixB)
-identity($matA_height) #pass a matrix height or width
-transpose(matrixA, $matA_height, $matA_width)
+#add(matrixA, matrixB)
+#subtract(matrixA, matrixB)
+#multiply(matrixA, matrixB)
+#identity($matA_height) #pass a matrix height or width
+#transpose(matrixA, $matA_height, $matA_width)

9e390c1be21dfac3afe3312de7d62854aaf6188f
Aldair <amartinez238@csub.edu>
2021-05-09 18:16:48 -0700

    Changed mult. function. Added scaler, determinant, inverse, power, and decimal conversion function

--- matrixCalculator.rb
+++ matrixCalculator.rb
@@ -84,28 +84,27 @@ end
 
 
 #fuction for matrix multiplication
-def multiply(matrixA, matrixB) #A's columns must equal B's rows
-  row, col = $matA_width, $matB_height
-      #create a 3rd matrix
-      #3rd matrix will have size of matrix A's column
-      # and matrix B's row.
-  matrixC = Array.new(matrixA.length){Array.new(matrixB[0].length)}
- 
-  for i in 0..col - 1
-    for j in 0..row - 1
-      matrixC[i][j] = 0  #set all to 0 initially
-    end
-  end
+def multiply(matrixA, matA_height, matA_width, matrixB, matB_height, matB_width)
+  #create a 3rd matrix
+  #3rd matrix will have size of matrix A's column
+  # and matrix B's row.
+matrixC = Array.new(matA_width){Array.new(matB_height)}
 
-  for i in 0..matrixC.length - 1
-    for j in 0..matrixC[0].length - 1
-      for k in 0..matrixA[0].length - 1
-        #multiplication of matrices is added to 3rd matrix
-        matrixC[i][j] += matrixA[i][k] * matrixB[k][j]
-      end
-    end
+for i in 0..matrixC.length - 1
+for j in 0..matrixC[0].length - 1
+  matrixC[i][j] = 0  #set all to 0 initially
+end
+end
+
+for i in 0..matrixC.length - 1
+for j in 0..matrixC[0].length - 1
+  for k in 0..matrixA[0].length - 1
+    #multiplication of matrices is added to 3rd matrix
+    matrixC[i][j] += matrixA[i][k] * matrixB[k][j]
   end
-  print "\nMatrix A * Matrix B\n", matrixC
+end
+end
+print "\nMatrix A * Matrix B\n", matrixC
 end
 
 
@@ -137,8 +136,41 @@ def transpose(matrix, height, width)
   print "\nTranspose of matrix ", matrixC,"\n"
 end
 
+#scaler function
+def scaler(matrix, mat_height, mat_width, input)
+  row, col = mat_width, mat_height
+  matrixC = Array.new(row){Array.new(col)}
+  for i in 0..col-1
+    for j in 0..row-1
+      #multiply by input and store in different matrix
+      matrixC[i][j] = matrix[i][j] * input  
+    end
+  end
+  print "\nMatrix index * #{input}\n", matrixC
+end 
 
 
+def exponent(matrixTmp, input)
+  # the double splat operator returns exponent of matrix
+  return *matrixTmp **input #splat operator make "Matrix" go away
+end
+
+#fraction to decimal function
+def to_Decimal(matrix, mat_height, mat_width)
+  row, col = mat_width, mat_height
+  matrixC = Array.new(row){Array.new(col)}
+  #loop through matrix
+  for i in 0..col-1
+    for j in 0..row-1
+      #change fraction to decimal and round to the 5th 
+      matrixC[i][j] = matrix[i][j].to_f.round(5) 
+    end
+  end
+  return matrixC
+end
+
+
+#Main_______________________________________________________________
 row, col = 0, 0
 index = 0
 
@@ -187,13 +219,60 @@ end
 #matrixA[0][0] = 66
 #matrixB[0][0] = 51
 
-#function calls
+#function calls_______________________________________________________________
 add(matrixA, matrixB)
 subtract(matrixA, matrixB)
-multiply(matrixA, matrixB)
+multiply(matrixA, $matA_height, $matA_width, matrixB, $matB_height, $matB_width)
 identity($matA_height) #pass a matrix height or width
 transpose(matrixA, $matA_height, $matA_width)
 
+print("\n\nInsert a value for scaler and pow function between 1 & 10: ")
+int_input = gets.chomp.to_i   #takes input
+if !(int_input.between?(1, 10))  #doe not accept letters
+  while !(int_input.between?(1, 10))
+    print("Input must be a number between 1 & 10: ")
+    int_input = gets.chomp.to_i
+  end
+end
+print("Input value is: #{int_input}")
+scaler(matrixA, $matA_height, $matA_width, int_input)
+
+require "matrix" #crucial for matrix operations to work
 
 
+#convert "Array" to "Matrix" because determinant function
+#only accepts "Matrix" types
+matrixTmpA = Matrix[*matrixA] # * is splat operator: it essentially
+#can be used to treat an array  as if it weren't an array, but 
+#rather as if its elements were individual elements/arguments.
+#Without the splat operator, ther would be an extra '[]' around matrixA
+matrixTmpB = Matrix[*matrixB]
 
+print "\n\n Determinant of matrix\n"
+matrix_detA = matrixTmpA.determinant()  #get result and store in variable
+print "\nMatrix determinant A: ", matrix_detA
+
+matrix_detB = matrixTmpB.determinant()
+print "\nMatrix determinant B: ", matrix_detB
+
+
+print "\n\n Inverse matrix\n"
+matrix_invA = matrixTmpA.inverse() #get result and store in new variable
+matrix_invA = *matrix_invA #covert from matrix back into array to print
+matrix_invA = to_Decimal(matrix_invA, $matA_height, $matA_width) #change fraction to decimal
+print "\nMatrix inverse A: ", matrix_invA
+print("\n\n")
+matrix_invB = matrixTmpB.inverse() #Do the same with matrix B
+matrix_invB = *matrix_invB #covert from matrix back into array to print
+matrix_invB = to_Decimal(matrix_invB, $matB_height, $matB_width) #change to decimal
+print "\nMatrix inverse B: ", matrix_invB
+
+
+print("\nPower/Exponent functions \n")
+matrix_powA = exponent(matrixTmpA, int_input) #call function and store result in new var.
+matrix_powA = *matrix_powA   #covert from matrix back into array. Indexes work now.
+print("\nExponent A: ", matrix_powA)  
+print("\n\n")
+matrix_powB = exponent(matrixTmpB, int_input) 
+matrix_powB = *matrix_powB   #covert from matrix back into array
+print("\nExponent B: ", matrix_powB) 
